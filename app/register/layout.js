2dd13f8f5da7d7e351ae624bfb4da67ed7d0f172
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:52:19 +0200

    Improve search and app metadata

---
+++ app/register/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Registracija",
+  description:
+    "Besplatno izradite račun naručitelja ili majstora na MojMeštar."
+};
+
+export default function RegisterLayout({ children }) {
+  return children;
+}
