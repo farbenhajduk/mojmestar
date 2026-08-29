667e3c81b65737dcae79a6abd3e22fda749678e2
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:50:39 +0200

    Improve search and app metadata

---
+++ app/favoriti/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Spremljeni majstori",
+  robots: {
+    index: false,
+    follow: false
+  }
+};
+
+export default function FavoritesLayout({ children }) {
+  return children;
+}
