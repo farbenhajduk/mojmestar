a156952b913d5c26b226fdb217c569713f46b596
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:51:03 +0200

    Improve search and app metadata

---
+++ app/login/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Prijava",
+  robots: {
+    index: false,
+    follow: true
+  }
+};
+
+export default function LoginLayout({ children }) {
+  return children;
+}
