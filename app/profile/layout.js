f7dbd0493779f6533d9c3557847e16a832caf979
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:51:47 +0200

    Improve search and app metadata

---
+++ app/profile/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Moj profil",
+  robots: {
+    index: false,
+    follow: false
+  }
+};
+
+export default function ProfileLayout({ children }) {
+  return children;
+}
