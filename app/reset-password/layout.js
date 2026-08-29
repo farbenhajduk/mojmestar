32e95296a7dff8b6cbc89a20e4abfbfe8f5f6ebf
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:52:34 +0200

    Improve search and app metadata

---
+++ app/reset-password/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Nova lozinka",
+  robots: {
+    index: false,
+    follow: false
+  }
+};
+
+export default function ResetPasswordLayout({ children }) {
+  return children;
+}
