b3aeab323e8c8b63f15a3bfac6174c9560d3778b
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:51:29 +0200

    Improve search and app metadata

---
+++ app/majstori/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Majstori",
+  description:
+    "Pretražite profile lokalnih majstora prema usluzi i lokaciji."
+};
+
+export default function MajstoriLayout({ children }) {
+  return children;
+}
