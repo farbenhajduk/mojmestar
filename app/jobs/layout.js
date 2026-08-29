8004510caa729d223b1c993abb3f3b551ffc1c53
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:50:55 +0200

    Improve search and app metadata

---
+++ app/jobs/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Poslovi",
+  description:
+    "Objavite posao ili pronađite dostupne poslove za majstore u Hrvatskoj."
+};
+
+export default function JobsLayout({ children }) {
+  return children;
+}
