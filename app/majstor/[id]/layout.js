737faf1d3611118e4201c23d7cd97db65e9b584f
farbenhajduk <info@farbenhajduk.de>
2026-08-29 16:51:21 +0200

    Improve search and app metadata

---
+++ app/majstor/[id]/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Profil majstora",
+  description:
+    "Pogledajte profil, usluge, ocjene i portfolio majstora."
+};
+
+export default function ProProfileLayout({ children }) {
+  return children;
+}
